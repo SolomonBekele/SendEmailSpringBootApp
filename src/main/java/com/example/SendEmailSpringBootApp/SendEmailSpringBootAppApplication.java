89743c60671d95077055e805da71b1ac22f69b49
solomon bekele <solubman28@gmail.com>
2025-01-27 21:47:05 +0300

    first commit

---
+++ src/main/java/com/example/SendEmailSpringBootApp/SendEmailSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SendEmailSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SendEmailSpringBootApp/SendEmailSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SendEmailSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
