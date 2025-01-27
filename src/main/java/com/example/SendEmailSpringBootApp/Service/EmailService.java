b56134b82360dd753789c3bfaf8ae6c4684ed3ec
solomon bekele <solubman28@gmail.com>
2025-01-27 22:39:10 +0300

    finalized

---
+++ src/main/java/com/example/SendEmailSpringBootApp/Service/EmailService.java
@@ -0,0 +1,6 @@
+package com.example.SendEmailSpringBootApp.Service;
+
+import org.springframework.web.multipart.MultipartFile;
+public interface EmailService {
+    String sendMail(MultipartFile[] file, String to,String[] cc, String subject, String body);
+}
